53c3290e41939fafa7bb7d8adab37194da729754
Mathew Tran <mathew.tran.dev@gmail.com>
2024-11-18 20:56:38 -0800

    add level 3

--- Script/Autoload/Definitions.gd
+++ Script/Autoload/Definitions.gd
@@ -1,8 +1,13 @@
 extends Node
 
-var bIsDebug = true
+var bIsDebug = false
+
+var DebugLevel = "res://Scenes/Forest/Level3.tscn"
 
 func _ready():
 	if bIsDebug == false: 
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	
+	if DebugLevel != "" and bIsDebug:
+		get_tree().change_scene_to_packed(load(DebugLevel))
